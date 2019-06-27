ef9ac5298f59dd5823f414cd846f8c6f1e451172
WinNguyen07 <winhy.nguyen@gmail.com>
2019-06-27 02:50:10 +0100

    stream

--- webRTCDemo/src/app.js
+++ webRTCDemo/src/app.js
@@ -4,28 +4,33 @@
  * and open the template in the editor.
  */
 const openStream = require('./openStream');
-const $ = require('jquery');
+
+openStream();
+
+
+
 
 //openStream;
 
-const Peer = require('simple-peer');
-
-const p = new Peer({ initiator: location.hash === '#1', trickle: false });
-
-p.on('signal', token => {   
-    $('#txtMySignal').val(JSON.stringify(token));
-});
-
-p.on('connect', () => {
-    setInterval(() => p.send(Math.random()), 2000);
-});
-
-p.on('data', data => console.log('Nhan du lieu: ' + data));
-
-$('#btnConnect').click(() => {
-    const frSignal = JSON.parse($('#txtFrSignal').val());
-    p.signal(frSignal);
-});
-
-console.log('Xin chao');
-
+//const Peer = require('simple-peer');
+//
+//const p = new Peer({ initiator: location.hash === '#1', trickle: false });
+//
+//p.on('signal', token => {   
+//    $('#txtMySignal').val(JSON.stringify(token));
+//});
+//
+//// Send + connect data
+////p.on('connect', () => {
+////    setInterval(() => p.send(Math.random()), 2000);
+////});
+////
+////p.on('data', data => console.log('Nhan du lieu: ' + data));
+////end
+//
+//
+//
+//$('#btnConnect').click(() => {
+//    const frSignal = JSON.parse($('#txtFrSignal').val());
+//    p.signal(frSignal);
+//});
--- webRTCDemo/src/openStream.js
+++ webRTCDemo/src/openStream.js
@@ -3,10 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+const playVideo = require('./playVideo');
+const Peer = require('simple-peer');
+const $ = require('jquery');
 
 function openStream(){
     navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-    .then(stream => playVideo(stream, 'myStream'))
+    .then(stream => {
+        playVideo(stream, 'myStream');        
+        const p = new Peer({ initiator: location.hash === '#1', trickle: false, stream: stream });
+        p.on('signal', token => {   
+            $('#txtMySignal').val(JSON.stringify(token));
+        });
+        
+        $('#btnConnect').click(() => {
+            const frSignal = JSON.parse($('#txtFrSignal').val());
+            p.signal(frSignal);
+        });
+        
+        p.on('stream', frStream => playVideo(frStream, 'frStream'));
+    })
     .catch(err => console.log(err)); 
 };
 
