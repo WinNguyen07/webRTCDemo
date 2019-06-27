295e10bb863c1adedbf5820042c6b6d04668d894
WinNguyen07 <winhy.nguyen@gmail.com>
2019-06-27 02:30:36 +0100

    send data

--- webRTCDemo/src/app.js
+++ webRTCDemo/src/app.js
@@ -10,11 +10,17 @@ const $ = require('jquery');
 
 const Peer = require('simple-peer');
 
-const p = new Peer({ initiator: location.hash === '#1', tricke: false });
+const p = new Peer({ initiator: location.hash === '#1', trickle: false });
 
 p.on('signal', token => {   
     $('#txtMySignal').val(JSON.stringify(token));
 });
+
+p.on('connect', () => {
+    setInterval(() => p.send(Math.random()), 2000);
+});
+
+p.on('data', data => console.log('Nhan du lieu: ' + data));
 
 $('#btnConnect').click(() => {
     const frSignal = JSON.parse($('#txtFrSignal').val());
